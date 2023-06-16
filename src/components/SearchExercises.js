f49611513024b2edc6bb09c5f71769a26971c082
thuhale2210 <thuhale2210@gmail.com>
2023-06-15 23:51:01 -0400

    First commit of some components + Update files

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,11 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
-const SearchExercises = () => {
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
+
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState('');
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
+        (item) => item.name.toLowerCase().includes(search)
+          || item.target.toLowerCase().includes(search)
+          || item.equipment.toLowerCase().includes(search)
+          || item.bodyPart.toLowerCase().includes(search),
+      );
+
+      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+
+      setSearch('');
+      setExercises(searchedExercises);
+    }
+  };
+
   return (
     <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb='50px' textAlign='center'>Awesome Exercises You Should Know</Typography>
-      <Box position='relative' mb='72px'>
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb='50px' textAlign='center'>
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
         <TextField
           sx={{
             input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
@@ -14,13 +50,12 @@ const SearchExercises = () => {
             borderRadius: '20px'
           }}
           height='76px'
-          value=''
-          onChange={(e) => { }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder='Search Exercises'
           type='text'
         />
-        <Button
-          className='search-btn'
+        <Button className='search-btn'
           variant='outlined'
           color='success'
           sx={{
@@ -32,12 +67,15 @@ const SearchExercises = () => {
             height: '56px',
             ml: '20px'
           }}
-        >
+          onClick={handleSearch}>
           Search
         </Button>
       </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+        <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
+      </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,16 @@
+// Connect with RapidAPI
+export const exerciseOptions = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+        'X-RapidAPI-Key': '7d4d1999a3msh8d171cb2f324d62p1de941jsnb30a5d5578c9',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+} 
