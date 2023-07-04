13b81476238b69b1d0e270c98968d5de94cbc657
thuhale2210 <thuhale2210@gmail.com>
2023-07-04 00:14:09 -0400

    Add Youtube API

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,6 +7,14 @@ export const exerciseOptions = {
     }
 };
 
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '2c84f76a8emsh7d67c32b97c8e00p13d87djsn30f178c5b05c',
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+};
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
