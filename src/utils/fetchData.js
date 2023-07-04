4c7a5e1745c2546aca48987a6464ac4c75c54902
thuhale2210 <thuhale2210@gmail.com>
2023-07-04 12:24:36 -0400

    Update API key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '2c84f76a8emsh7d67c32b97c8e00p13d87djsn30f178c5b05c',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
@@ -10,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '2c84f76a8emsh7d67c32b97c8e00p13d87djsn30f178c5b05c',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
 };
