fd4ee0c16e32c9c8cb3b126bb9a0bf3a1340dd23
thuhale2210 <thuhale2210@gmail.com>
2023-10-11 16:13:08 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Typography, Stack } from '@mui/material'
 
-import Logo from '../assets/images/Logo.png'
-
 const Footer = () => {
   return (
     <Stack sx={{ alignItems: 'center' }}>
       <Typography sx={{ fontSize: { lg: '15px', xs: '10px' } }} mb='20px'>
-        Made in 2023. Click 'Home' to enter the website.
+        Made in 2023
       </Typography>
     </Stack>
 
