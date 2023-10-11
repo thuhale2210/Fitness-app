aa80551d1851da4c6a98c980ad7c6953b26ec985
thuhale2210 <thuhale2210@gmail.com>
2023-10-11 10:46:27 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => (
   <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      <Route path="/Fitness-app/" element={<Home />} />
+      <Route path="/Fitness-app/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
     <Footer />
   </Box>
